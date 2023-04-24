226231b88b88009758775377739ef458da567f99
vtecftwy <github@procurasia.com>
2023-04-24 16:27:49 +0800

    correct ref to package root

--- metagentools/__init__.py
+++ metagentools/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.0.6"
+
+# import os
+# from metagentools import __file__
+
+# from metagentools import __file__
+# ROOT = os.path.split(os.path.split(__file__)[0])[0]
+# os.environ['METAGENTOOLS_ROOT'] = ROOT
